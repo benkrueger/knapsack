96fa26d0be65938c4066bca92bc233127039d5b5
Ben Krueger <bokkw4@mst.edu>
2020-01-30 15:19:12 -0500

    switch 2 go

---
+++ main.go
@@ -0,0 +1,14 @@
+package knapsack
+import (
+	"fmt"
+	"flag"
+)
+/*
+Knapsack
+Dynamic programming download optimizer/manager.
+knapsack [-l] <config file> [-d](dry run) -h <rtorrent host:port> [-i(interactive config)]
+-t <tracker annouce:port> -r <rss address:port> -j <jackett address:port> -b <bandwidth limit>
+*/
+func main(){
+
+}
